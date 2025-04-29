645c53fc47ec39a38d510eab819bb2ea8597e33b
Jailson <jailsonrodriguestech@gmail.com>
2025-04-28 23:52:30 -0300

    test(app): initial test commit for the app setup

---
+++ AppStyles.ts
@@ -0,0 +1,27 @@
+// stylesApp.ts
+import { StyleSheet, ViewStyle, TextStyle } from 'react-native';
+
+export const AppStyles = StyleSheet.create<{
+  container: ViewStyle;
+  text: TextStyle;
+  equipe: TextStyle;
+}>({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginTop:100,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+
+  },
+  equipe:{
+    fontSize:20,
+    fontFamily: 'Times New Roman',
+    textAlign:'center',
+  },
+
+});
