b2a7a2a0f4f122d10f7b5d411e39250c6b5e82a3
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-05-03 00:57:51 -0300

    refactor(App): removes the TaskList component and integrates the App Navigator as in the first commit
    - avoids conflicts

--- App.tsx
+++ App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import TaskList from './src/components/TaskItem/TaskList'; 
+import AppNavigator from './src/Navigation'; // Importa a navegação principal
+
 
 const App: React.FC = () => {
-  return (
-    <View style={styles.area}>
-      <TaskList />
-    </View>
-  );
+  return <AppNavigator />;
 };
 
-const styles = StyleSheet.create({
-  area: {
-    flex: 1,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
 export default App;
+
+//Neiva
+//Coloquei uma página de acesso para a partir dela acessar as outras páginas
+//  Poderá ser acrescetado outros botões para as páginas login, cadastro, etc...
+// Assim todos poderão ver o andamento do projeto acessando as páginas uns dos outros pelo botão de link.
+// Isso pode ser feito na página navigation
+// na página que você criar, coloca um botão de retorno à página navegação, se quiser
