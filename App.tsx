5e05e0f2e9ac4307264e6807869d2fa40adafe81
Diogo Souza <dooss2085@gmail.com>
2025-05-03 20:19:35 -0300

    fix: removes unnecessary comment from App.tsx

--- App.tsx
+++ App.tsx
@@ -8,7 +8,6 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const init = async () => {
-      // …do multiple sync or async tasks
     };
 
     init().finally(async () => {
@@ -21,16 +20,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-/*<<<<<<< HEAD
-
-=======
-//Neiva
-//Coloquei uma página de acesso para a partir dela acessar as outras páginas
-//  Poderá ser acrescetado outros botões para as páginas login, cadastro, etc...
-// Assim todos poderão ver o andamento do projeto acessando as páginas uns dos outros pelo botão de link.
-// Isso pode ser feito na página navigation
-// na página que você criar, coloca um botão de retorno à página navegação, se quiser
->>>>>>> develop
-
-*/
