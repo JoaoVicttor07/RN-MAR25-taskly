2b18646662e99ce900861f819e599a2906c2cda0
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-05-04 04:19:38 -0300

    refactor: define FontStyle interface and correct fontWeight property in Fonts object

--- src/Theme/fonts.ts
+++ src/Theme/fonts.ts
@@ -1,4 +1,19 @@
-const Fonts = {
+import { TextStyle } from 'react-native';
+
+interface FontStyle {
+  fontFamily: string;
+  fontSize: number;
+  fontWeight: TextStyle['fontWeight'];
+}
+
+const Fonts: {
+  Roboto70048: FontStyle;
+  Roboto70024: FontStyle;
+  Roboto60020: FontStyle;
+  Roboto50018: FontStyle;
+  Roboto40016: FontStyle;
+  Roboto40012: FontStyle;
+} = {
   Roboto70048: {
     fontFamily: 'Roboto-Bold',
     fontSize: 48,
@@ -12,7 +27,7 @@ const Fonts = {
   Roboto60020: {
     fontFamily: 'Roboto-SemiBold',
     fontSize: 20,
-    fontweight: '600',
+    fontWeight: '600',
   },
   Roboto50018: {
     fontFamily: 'Roboto-Medium',
