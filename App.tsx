47a45b6ed4b71cce07ed357a2cf75cf72533ccbb
Diogo Souza <dooss2085@gmail.com>
2025-05-05 01:09:23 -0300

    feat: implement initial splash screen (second attempt)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import AppNavigator from './src/Navigation'; // Importa a navegação principal
+import { useEffect } from 'react';
+import BootSplash from 'react-native-bootsplash';
 
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
+
   return <AppNavigator />;
 };
 
