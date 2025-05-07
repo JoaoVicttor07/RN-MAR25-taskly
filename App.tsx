d32e8bc466e79459689014951750e26cf9aee248
Diogo Souza <dooss2085@gmail.com>
2025-05-07 04:34:42 -0300

    feat(bugFixed): remove react-native-bootsplash dependency and delete tsconfig.json file

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
 import AppNavigator from './src/Navigation/index'; // Importa a navegação principal
-import { useEffect } from 'react';
-import BootSplash from 'react-native-bootsplash';
 import { ThemeProvider } from './src/Theme/ThemeContext'; // Importa o provedor de tema
 
 const App: React.FC = () => {
-
-  useEffect(() => {
-    const init = async () => {
-      // …do multiple sync or async tasks
-    };
-
-    init().finally(async () => {
-      await BootSplash.hide({ fade: true });
-      console.log('BootSplash has been hidden successfully');
-    });
-  }, []);
 
   return (
     <ThemeProvider> {/* Envolve o aplicativo com o provedor de tema */}
