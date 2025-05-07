bfd4ec25bd644ae96caad86d1f5943276e27ef4d
Jailson <jailsonrodriguestech@gmail.com>
2025-05-07 08:59:04 -0300

    chore: replace file env.ts to root

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,13 +1,18 @@
 //useApi.ts
 
 import axios from 'axios';
-import { API_BASE_URL } from '../env';
+import {API_BASE_URL} from '../../env';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-export const registerUser = async (data: { email: string; password: string; name: string; phone_number: string }) => {
+export const registerUser = async (data: {
+  email: string;
+  password: string;
+  name: string;
+  phone_number: string;
+}) => {
   return api.post('/auth/register', data);
 };
 
